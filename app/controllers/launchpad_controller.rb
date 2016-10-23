0d5bbe1820143f20fd11b5cfb23534cf8036a337
Vincent Daubry <vdaubry@gmail.com>
2016-10-23 14:47:34 +0200

    random typing speed

--- app/controllers/launchpad_controller.rb
+++ app/controllers/launchpad_controller.rb
@@ -1,6 +1,6 @@
 class LaunchpadController < ApplicationController
   def show
     @game = Game.new
-    @random_names = 10.times.map { Haikunator.haikunate(0) }
+    @random_names = 10.times.map { "#{Haikunator.haikunate(0)}^#{(500 + rand(500)).to_s}" }
   end
 end
