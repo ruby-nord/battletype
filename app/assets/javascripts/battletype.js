03162f690e774fa483b91ba40a76169b6e798eb9
Cecile Veneziani <contact@cecilitse.org>
2016-10-24 00:37:51 +0200

    Add log system to game page

--- app/assets/javascripts/battletype.js
+++ app/assets/javascripts/battletype.js
@@ -1,6 +1,7 @@
 /* global Stdin, PS2EventRelay, CombatZone, Dockyard, Ship, LifeOMeter */
 
 //= require battletype/stdin
+//= require battletype/logs
 //= require battletype/ps2_event_relay
 //= require battletype/combat_zone
 //= require battletype/dockyard
@@ -30,35 +31,40 @@
       Dockyard.registerTemplate("mothership", document.getElementById("mothership_template"));
 
       this.mothership = Dockyard.launchMothership(this.$combatZone);
-      
+
       this.playerLifeOMeter   = LifeOMeter.activate(document.getElementById("life_player"));
       this.opponentLifeOMeter = LifeOMeter.activate(document.getElementById("life_opponent"));
-      
+
       this._eventsRelay.addEventListener("entry", function (e) { this.transmitEntry(e.detail); }.bind(this), false);
       this._eventsRelay.addEventListener("switchMode", function (e) { this.switchMode(e.detail); }.bind(this), false);
       this._eventsRelay.addEventListener("bombDropped", function (e) { this._transmitBombing(e.detail); }.bind(this), false);
 
       // TODO: factory pattern
+      this._logs = Object.create(Logs, {
+        boxTag: { value: document.getElementById("log_system") },
+      });
+
       this._stdin = Object.create(Stdin, {
-        inputDevice: { value: document.getElementById("stdin") },
-        ps2Port: { value: this._eventsRelay }
+        inputDevice:  { value: document.getElementById("stdin") },
+        ps2Port:      { value: this._eventsRelay }
       });
       this._stdin.powerOn();
-
     },
     incomingTransmission: function (payload) {
       switch(payload.code) {
       case "successful_attack":
-        if (payload.player_id != this.playerId) { // TODO: comparer plutôt à opponentId
+        if (payload.player_id == this.playerId) {
+          this._logs.displayMessage(payload.code);
+        }
+        else { // TODO: comparer plutôt à opponentId
           audio_spaceship("right");
           Dockyard.launch({ word: payload.word, ship: payload.launched_ship }, this.$combatZone, this._eventsRelay);
-        } else {
-          // TODO
         }
-        
         break;
       case "failed_attack":
         if (payload.player_id == this.playerId) {
+          this._logs.displayMessage(payload.code, payload.error_codes);
+
           console.log("Failed attack with word", payload.word);
           console.log("Error code", payload.error_codes);
         } else {
@@ -69,19 +75,36 @@
         if (payload.player_id == this.playerId) {
           var ship = Ship.locate(payload.word);
           if (ship) { this.$combatZone.get(0).removeChild(ship); }
+
+          this._logs.displayMessage(payload.code);
           // TODO: update gauge & unlock strike
         } else {
           // TODO
         }
         break;
+      case "failed_bombing":
+        if (payload.player_id == this.playerId) {
+          this._logs.displayMessage(payload.code, payload.error_codes);
+        }
+        break;
+      case "failed_defense":
+        if (payload.player_id == this.playerId) {
+          this._logs.displayMessage(payload.code, payload.error_codes);
+        }
+        break;
       case "successful_bombing":
-        if (payload.played_id == this.playerId) {
+        this._logs.displayMessage(payload.code);
+
+        if (payload.player_id == this.playerId) {
+          console.info(payload.code);
           this.opponentLifeOMeter.life = payload.bombed_mothership.life;
         } else {
           this.playerLifeOMeter.life = payload.bombed_mothership.life;
         }
         break;
       case "game_won":
+        this._logs.displayMessage(payload.code);
+
         if (payload.player_id == this.playerId) {
           // TODO
           console.log("\o/ You won!");
@@ -94,13 +117,9 @@
       case "player_joined":
         if (payload.player_id != this.playerId) {
           var nickname = payload.nickname;
-          
-          $("#log_system > p")
-            .text(nickname + " has joined the game!")
-            .delay(1500)
-            .fadeOut("slow"); // TODO tell-dont-ask & remove jQuery
-          
+
           this.$combatZone.find("#opponent_nickname").text(nickname); // TODO tell-dont-ask
+          this._logs.displayMessage(payload.code);
         }
         break;
       }
@@ -129,7 +148,7 @@
     },
     switchMode: function () {
       Battletype.attacking = !Battletype.attacking;
-      
+
       if (Battletype.attacking) {
         this.$combatZone.removeClass("defense_mode").addClass("attack_mode"); // TODO tell-dont-ask
         this._stdin.indicateAttackMode();
