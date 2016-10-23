a87fd14e93aef8afa7755875c20a81c2ed040797
Vincent Daubry <vdaubry@gmail.com>
2016-10-23 22:08:26 +0200

    remove background audio

--- app/assets/javascripts/sounds.js
+++ app/assets/javascripts/sounds.js
@@ -1,16 +1,3 @@
-var ambiance;
-
-var ready = function () {
-  ambiance = new Howl({
-    src: ['/sounds/ambiance.mp3'],
-    loop: true,
-    autoplay: true
-  });
-}
-
-$(document).ready(ready);
-$(document).on("page:load", ready);
-
 function audio_hit() {
   playAudio('hit.mp3', 0);
 }
@@ -27,14 +14,6 @@ function audio_spaceship(position) {
     x = 1;
   }
   playAudio('spaceship.mp3', x);
-}
-
-function audio_playAmbiance() {
-  ambiance.play();
-}
-
-function audio_pauseAmbiance() {
-  ambiance.pause();
 }
 
 function playAudio(audioFile, x) {
