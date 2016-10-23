70ea1d3b0849372a3d0136482cdae0c3ac3160e8
Ronan Limon Duparcmeur <ronan@2-45.pm>
2016-10-24 01:30:19 +0200

    Un dernier pour Seb

--- app/assets/javascripts/battletype/combat_zone.js
+++ app/assets/javascripts/battletype/combat_zone.js
@@ -3,7 +3,7 @@
     _properties: {
       randomFreeVerticalSlot: {
         get: function () {
-          return Math.floor(Math.random() * this.height() - 80) + 80;
+          return Math.floor(Math.random() * this.height()) - 80;
         }
       },
       ships: {
