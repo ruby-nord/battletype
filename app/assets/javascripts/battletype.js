362ffd68713db31f3e332a0c0f30ff94cbd3483f
Sébastien Wozny <wozny.s@gmail.com>
2016-10-23 21:17:46 +0200

    Ajout d'icones attaque / défense dans l'input

--- app/assets/javascripts/battletype.js
+++ app/assets/javascripts/battletype.js
@@ -9,37 +9,37 @@
 (function () {
   this.Battletype = {
     attacking: true,
-    
+
     _eventsRelay: Object.create(PS2EventRelay),
-    
+
     init: function(options) {
       this.playerId         = options.playerId;
       this.opponentId       = options.playerId;
-      
+
       this.$combatZone       = CombatZone.locate();
-      
+
       this.attackFrequency  = options.attackFrequency;
       this.defenseFrequency = options.defenseFrequency;
       this.bombingFrequency = document.getElementById("bombing_frequency");
-      
+
       Dockyard.ps2Port = this._eventsRelay;
       Dockyard.registerTemplate("small", document.getElementById("small_ship_template"));
       Dockyard.registerTemplate("medium", document.getElementById("medium_ship_template"));
       Dockyard.registerTemplate("large", document.getElementById("large_ship_template"));
       Dockyard.registerTemplate("mothership", document.getElementById("mothership_template"));
-      
+
       this.mothership = Dockyard.launchMothership(this.$combatZone);
-      
+
       this._eventsRelay.addEventListener("entry", function (e) { this.transmitEntry(e.detail); }.bind(this), false);
       this._eventsRelay.addEventListener("switchMode", function (e) { this.switchMode(e.detail); }.bind(this), false);
       this._eventsRelay.addEventListener("bombDropped", function (e) { this._transmitBombing(e.detail); }.bind(this), false);
-      
+
       this._stdin = Object.create(Stdin, {
         inputDevice: { value: options.inputDevice },
         ps2Port: { value: this._eventsRelay }
       });
       this._stdin.powerOn();
-      
+
     },
     incomingTransmission: function (payload) {
       switch(payload.code) {
@@ -70,22 +70,22 @@
     },
     _transmitAttack: function (entry) {
       this.attackFrequency.querySelector("[name='word']").value = entry.word;
-      
+
       $(this.attackFrequency).trigger("submit.rails");
     },
     _transmitDefense: function (entry) {
       this.defenseFrequency.querySelector("[name='word']").value = entry.word;
       this.defenseFrequency.querySelector("[name='perfectTyping']").value = entry.perfectTyping;
-      
+
       $(this.defenseFrequency).trigger("submit.rails");
     },
     _transmitBombing: function (ship) {
       ship.className += " leaving"; // The bombing ship leaves the scene
       this.mothership.hit = true;   // The mothership takes a hit
-      
+
       this.bombingFrequency.elements["word"].value = ship.word;
       $(this.bombingFrequency).trigger("submit.rails");
-      
+
       console.log("_transmitBombing", ship);
     },
     switchMode: function () {
@@ -93,9 +93,11 @@
       console.log("Switched mode to Battletype.attacking = "+Battletype.attacking);
       if (Battletype.attacking) {
         this.$combatZone.removeClass("defense_mode").addClass("attack_mode");
+        $("#stdin").removeClass("defense").addClass("attack");
       }
       else {
         this.$combatZone.removeClass("attack_mode").addClass("defense_mode");
+        $("#stdin").removeClass("attack").addClass("defense");
       }
     },
   };
