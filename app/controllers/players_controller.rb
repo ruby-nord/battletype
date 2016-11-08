f9764cd9fbb42346c0c094bd797138060a5ba8ec
Cecile Veneziani <contact@cecilitse.org>
2016-11-08 23:55:01 +0100

    [hysteria] Cleanup player update

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,13 +2,14 @@ class PlayersController < ApplicationController
   before_action :set_player
 
   def update
-    return render json: {error: "Cannot update another user"}, status: 401 if @player != current_player
+    return render json: { error: "Cannot update another user" }, status: 401 if @player != current_player
 
     @player.update(player_params)
-    render json: {status: :ok}, status: 200
+    render json: { status: :ok }, status: 200
   end
 
   private
+
   def set_player
     @player = Player.find(params[:id])
   end
