843e9a534ba53cd97e1793b24301c8d1901128ac
Cecile Veneziani <contact@cecilitse.org>
2017-10-31 18:10:45 +0100

    Use HOST env variable on homepage game creation form

--- app/controllers/launchpad_controller.rb
+++ app/controllers/launchpad_controller.rb
@@ -1,6 +1,8 @@
 class LaunchpadController < ApplicationController
   def show
+    @host = ENV.fetch('HOST')
     @game = Game.new
+
     @random_names = 10.times.map { "#{Haikunator.haikunate(0)}^#{(500 + rand(500)).to_s}" }
   end
 end
