dc0078bb69d3f1e347705132d6dcc384c3bf3199
Cecile Veneziani <contact@cecilitse.org>
2018-11-05 16:52:50 +0100

    Upate database schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,63 +16,63 @@ ActiveRecord::Schema.define(version: 20161023171219) do
   enable_extension "plpgsql"
 
   create_table "active_admin_comments", force: :cascade do |t|
-    t.string   "namespace"
-    t.text     "body"
-    t.string   "resource_id",   null: false
-    t.string   "resource_type", null: false
-    t.string   "author_type"
-    t.integer  "author_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
-  end
-
-  create_table "games", force: :cascade do |t|
-    t.string   "name"
-    t.string   "state"
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_id", null: false
+    t.string "resource_type", null: false
+    t.string "author_type"
+    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "slug"
-    t.index ["slug"], name: "index_games_on_slug", unique: true, using: :btree
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
   end
 
-  create_table "players", force: :cascade do |t|
-    t.string   "nickname"
-    t.integer  "life"
-    t.integer  "strike_gauge",    default: 0
-    t.string   "unlocked_strike"
-    t.boolean  "human",           default: false
-    t.boolean  "creator",         default: false
-    t.boolean  "won",             default: false
-    t.integer  "game_id"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.string   "last_ip"
-    t.index ["game_id"], name: "index_players_on_game_id", using: :btree
-  end
-
-  create_table "ships", force: :cascade do |t|
-    t.integer  "damage"
-    t.float    "velocity"
-    t.string   "state"
-    t.integer  "player_id"
-    t.integer  "word_id"
+  create_table "games", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "ship_type"
-    t.index ["player_id"], name: "index_ships_on_player_id", using: :btree
-    t.index ["word_id"], name: "index_ships_on_word_id", using: :btree
+    t.string "slug"
+    t.index ["slug"], name: "index_games_on_slug", unique: true
   end
 
-  create_table "words", force: :cascade do |t|
-    t.string   "value"
-    t.integer  "game_id"
+  create_table "players", id: :serial, force: :cascade do |t|
+    t.string "nickname"
+    t.integer "life"
+    t.integer "strike_gauge", default: 0
+    t.string "unlocked_strike"
+    t.boolean "human", default: false
+    t.boolean "creator", default: false
+    t.boolean "won", default: false
+    t.integer "game_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["game_id"], name: "index_words_on_game_id", using: :btree
-    t.index ["value"], name: "index_words_on_value", using: :btree
+    t.string "last_ip"
+    t.index ["game_id"], name: "index_players_on_game_id"
+  end
+
+  create_table "ships", id: :serial, force: :cascade do |t|
+    t.integer "damage"
+    t.float "velocity"
+    t.string "state"
+    t.integer "player_id"
+    t.integer "word_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "ship_type"
+    t.index ["player_id"], name: "index_ships_on_player_id"
+    t.index ["word_id"], name: "index_ships_on_word_id"
+  end
+
+  create_table "words", id: :serial, force: :cascade do |t|
+    t.string "value"
+    t.integer "game_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_words_on_game_id"
+    t.index ["value"], name: "index_words_on_value"
   end
 
   add_foreign_key "players", "games"
